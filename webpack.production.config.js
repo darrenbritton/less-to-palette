76e576b2854c78d74c2039ae328ae57bb2d35b74
Darren Britton <darren.britton@mydit.ie>
2017-03-02 13:08:36 +0000

    Create webpack.production.config.js

---
+++ webpack.production.config.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+
+const settings = {
+  entry: {
+    bundle: [
+      "react-hot-loader/patch",
+      "./src/index.js"
+    ]
+  },
+  output: {
+    filename: "bundle.js",
+    publicPath: "/",
+    path: __dirname
+  },
+  resolve: {
+    extensions: [ '.js', '.jsx', '.json', '.css']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ["es2015", { modules: false }],
+            "stage-2",
+            "react"
+          ],
+          plugins: [
+            "transform-node-env-inline"
+          ],
+          env: {
+            development: {
+              plugins: ["react-hot-loader/babel"]
+            }
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: "[name]--[local]--[hash:base64:8]"
+            }
+          },
+          "postcss-loader" // has separate config, see postcss.config.js nearby
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('common.js'),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ],
+};
+
+module.exports = settings;
